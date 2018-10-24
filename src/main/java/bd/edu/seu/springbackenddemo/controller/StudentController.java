575c6040fdf9d2730935611eae3d71bc6e5331cd
shihab122 <shihabhossain611@gmail.com>
2018-10-24 07:30:38 +0600

    Adding test cases

--- src/main/java/bd/edu/seu/springbackenddemo/controller/StudentController.java
+++ src/main/java/bd/edu/seu/springbackenddemo/controller/StudentController.java
@@ -3,6 +3,11 @@ package bd.edu.seu.springbackenddemo.controller;
 import bd.edu.seu.springbackenddemo.model.Student;
 import bd.edu.seu.springbackenddemo.service.StudentService;
 import lombok.AllArgsConstructor;
+<<<<<<< HEAD
+=======
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+>>>>>>> Adding test cases
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,13 @@ public class StudentController {
         return studentService.getAllStudents();
     }
 
+<<<<<<< HEAD
+=======
+    @GetMapping(value = "/student/rall")
+    public ResponseEntity<List<Student>> getAllStudents1() {
+        return new ResponseEntity<List<Student>>(studentService.getAllStudents(), HttpStatus.OK);
+    }
+
+>>>>>>> Adding test cases
     // write code to test this service
 }
--- src/test/java/bd/edu/seu/springbackenddemo/SpringBackendDemoApplicationTests.java
+++ src/test/java/bd/edu/seu/springbackenddemo/SpringBackendDemoApplicationTests.java
@@ -1,16 +1,78 @@
 package bd.edu.seu.springbackenddemo;
 
+<<<<<<< HEAD
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+=======
+import bd.edu.seu.springbackenddemo.model.Student;
+import bd.edu.seu.springbackenddemo.repository.StudentRepository;
+import bd.edu.seu.springbackenddemo.service.StudentService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+
+
+>>>>>>> Adding test cases
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBackendDemoApplicationTests {
 
+<<<<<<< HEAD
     @Test
     public void contextLoads() {
     }
 
+=======
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private StudentService studentService;
+
+
+    private Student std;
+    private List<Student> studentList = new ArrayList<>();
+
+    @Before
+    public void initializing(){
+        std = new Student(1234, "Shihab", 3.12);
+        //studentRepository.save(std);
+        studentList = studentService.getAllStudents();
+
+    }
+
+    @Test
+    public void testStudent(){
+        String name = "shihab";
+        assertThat(std.getName()).as("After create a new student expect:%s Found:%s",name, std.getName()).isEqualTo(name);
+        assertThat(std.getId()).isEqualTo(1234).isBetween(1000, 10000);
+        assertThat(std.getCgpa()).isEqualTo(3.12).isBetween(2.00, 4.00);
+    }
+
+
+    //@Ignore("studentList.size() might be smaller or greater than 3")
+    @Test
+    public void testStudentList(){
+        Student ts = null;
+        assertThat(studentList).hasSize(4).contains(std).doesNotContain(ts);
+    }
+
+    @Test
+    public void findStudentById(){
+        Student found = studentService.getStudent(1234);
+        assertThat(found).isEqualTo(std);
+    }
+
+
+>>>>>>> Adding test cases
 }
